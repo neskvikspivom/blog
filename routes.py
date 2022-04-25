95e17b91a54ca5b05e9b00e04a76eac27138b75d
demidraksin <greatraksin@icloud.com>
2022-04-25 18:28:22 +0300

    Finishing login function

--- routes.py
+++ routes.py
@@ -1,9 +1,20 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
+from forms import LoginForm
 
 
 @app.route('/')
 def main_page():
-    return '<h1>Hello!</h1>'
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():  # если форма отправляется
+        flash(f'Login requested for user {form.username.data}')
+        return redirect(url_for('main_page'))
+    return render_template('login.html', title='Login page', form=form)
 
 
 
