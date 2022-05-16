403baadda9b05359157e51ddc44f9684782914e1
neskvikspivom <ilya.fedotko09@gmail.com>
2022-05-16 18:29:38 +0300

    Adding page only for logged people

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for
 from app import app, login
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from forms import LoginForm
 from models import User
 
@@ -15,7 +15,7 @@ def main_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated():
+    if current_user.is_authenticated:
         return redirect(url_for('main_page'))
     form = LoginForm()
     if form.validate_on_submit():# если форма отправляется
@@ -33,5 +33,12 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('main_page'))
+
+
+
+@app.route('/support')
+@login_required
+def support():
+    return render_template('support.html')
 
 
