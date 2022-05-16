cb4ade277ff40114d7f5a71ad4136270d41630cb
neskvikspivom <ilya.fedotko09@gmail.com>
2022-05-16 17:51:23 +0300

    Adding user loader, keeping password function

--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 from app import db
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     """Описание таблицы пользователей в БД"""
     id = db.Column(db.Integer, primary_key=True)  # id - ключ таблицы, хранящий числа, используемый для идентификации
     username = db.Column(db.String(64), unique=True, index=True)  # уникальный юзернейм, по которому можно найти пользователя
@@ -10,6 +12,12 @@ class User(db.Model):
     password = db.Column(db.String(256))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     # связь(Таблица_Posts, обращение_author, динамический_метод_загрузки)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 class Post(db.Model):
--- routes.py
+++ routes.py
@@ -1,7 +1,11 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+from app import app, login
 from forms import LoginForm
+from models import User
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route('/')
 def main_page():
