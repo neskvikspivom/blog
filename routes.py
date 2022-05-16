62f9715921d3147b032e5bff74c94e06a9146c92
neskvikspivom <ilya.fedotko09@gmail.com>
2022-05-16 18:10:25 +0300

    Adding login and logout functions

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, flash, redirect, url_for
 from app import app, login
+from flask_login import current_user, login_user, logout_user
 from forms import LoginForm
 from models import User
 
@@ -14,11 +15,23 @@ def main_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated():
+        return redirect(url_for('main_page'))
     form = LoginForm()
-    if form.validate_on_submit():  # если форма отправляется
-        flash(f'Login requested for user {form.username.data}')
+    if form.validate_on_submit():# если форма отправляется
+        user = User.query.filter_by(username=form.username.data)
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('Login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('main_page'))
     return render_template('login.html', title='Login page', form=form)
 
+
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main_page'))
 
 
